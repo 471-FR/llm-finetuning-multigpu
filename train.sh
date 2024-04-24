f8d80b266604aa23b1034dee15639328164ab376
sylvain471 <sylvain.d@471.fr>
2024-04-24 20:46:05 +0200

    fix train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=4 ./run_fsdp_qlora.py --config ./llama_3_8B_fsdp_qlora.yaml
 
 # shutdown ec2 instance to avoid extra charges
-sudo shutdown -h now
+#sudo shutdown -h now
